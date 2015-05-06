2d35423bc0a8f64eb1c03307a5a347e0e475de0e
Jose Nunez <jose.nunez@intel.com>
2015-05-05 21:45:07 -0600

    improvements to distance sensor

--- smart_home_security/laser_beam_sensor/maker_faire_smart_home_security/maker_faire_smart_home_security.ino
+++ smart_home_security/laser_beam_sensor/maker_faire_smart_home_security/maker_faire_smart_home_security.ino
@@ -32,7 +32,96 @@
 #define ALARM_STATUS_WARNING 1
 #define ALARM_STATUS_PANIC   2
 
-#define alarmDurationMillis 2000
+#define alarmDurationMillis 3000
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
 
 boolean  debugMode          = false; /*Use true for DEBUG MODE ACTIVE, and false for DEBUG MODE INACTIVE*/
 
@@ -45,10 +134,6 @@ int Laser1DurationInMilliseconds = 0;
 int Laser2DurationInMilliseconds = 0;
 int Laser1Status = 1;
 int Laser2Status = 1;
-
-
-
-
 
 long previousLaserDuration = 0;
 
@@ -74,6 +159,7 @@ String sensorStatus = "01-UNSTABLE_GREEN"; /*01-UNSTABLE_GREEN, 02-STABLE_GREEN,
 
 
 void setup() {
+    tune();
     delay(2000);
     Serial.begin (9600);
     Serial.println("SETTING SKETCH UP...");
@@ -92,11 +178,14 @@ void setup() {
 boolean isFirstLoop = true;
 int alarmStatus_PREV = -1;
 String previousFrontDoorStatus = "";
+long lastPVCloudUpdateMillis = 0;
 void loop(){
   if(isFirstLoop) {
     Serial.println("INITIAL LOOP BEGIN");
-    String jsonvalue = buildPVCloudValue("S", "H","H", "C", "H");
+    String jsonvalue = buildPVCloudValue("S", "H","H", "C", "H","H","H");
     sendDataToPVCloud("HOUSE_STATUS",jsonvalue, false);
+    
+    //isFirstLoop flag is cleared out at the end of the loop() function... not here.
   }
    
   readSensors(false); 
@@ -105,10 +194,12 @@ void loop(){
   String jsonvalue = "";
   
   String laserStatus = (Laser1Status == LASER_BEAM_ABSENT && Laser2Status == LASER_BEAM_ABSENT)?"A":"S";
-  String z1Status = AbsenseSensor?"H":"L"; 
+  String z1Status = AbsenseSensor==false?"L":"H"; 
   String z2Status = PresenseSensor?"L":"H";
   String houseStatus = "";
-  String frontDoorStatus = DistanceInCM > 70?"H":"L";     
+  String frontDoorStatus = DistanceInCM < 70?"L":"H";     
+  String lightStatus = LightSensor > 500? "L": "H";
+  String temperatureStatus = TemperatureInCelsius > 30? "L": "H";
   
   
   
@@ -122,8 +213,9 @@ void loop(){
       case ALARM_STATUS_CLEAR:
         Serial.println("ALARM STATUS: CLEAR");
         houseStatus="C";
-        jsonvalue = buildPVCloudValue(laserStatus, z1Status,z2Status, houseStatus, frontDoorStatus);
+        jsonvalue = buildPVCloudValue(laserStatus, z1Status,z2Status, houseStatus, frontDoorStatus, temperatureStatus, lightStatus);
         sendDataToPVCloud("HOUSE_STATUS",jsonvalue, false);
+        lastPVCloudUpdateMillis = millis();
         break;
       case ALARM_STATUS_WARNING:
         Serial.println("ALARM STATUS: WARNING!");
@@ -131,30 +223,40 @@ void loop(){
       case ALARM_STATUS_PANIC:
         Serial.println("ALARM STATUS: PANIC!!!!!");
         houseStatus="U";
-        jsonvalue = buildPVCloudValue(laserStatus, z1Status,z2Status, houseStatus, frontDoorStatus);
+        jsonvalue = buildPVCloudValue(laserStatus, z1Status,z2Status, houseStatus, frontDoorStatus, temperatureStatus, lightStatus);
         sendDataToPVCloud("HOUSE_STATUS",jsonvalue, false);
+        lastPVCloudUpdateMillis = millis();
         break;
     }  
   } else if ( alarmStatus_PREV == -1 ) {//Home109080363
     alarmStatus_PREV = alarmStatus;
+    
   }
   
   Alarm_ExecuteStatus(alarmStatus);  
+  
   
   
   if(previousFrontDoorStatus == ""){
      Serial.print("INITIALIZING FRONTDOOR PREV: ");
      Serial.println(frontDoorStatus);
      previousFrontDoorStatus = frontDoorStatus;
-  } else if (frontDoorStatus != previousFrontDoorStatus){
+  } else if (frontDoorStatus != previousFrontDoorStatus && millis()-lastPVCloudUpdateMillis >5000){
+    if(frontDoorStatus=="L") tune();
      Serial.print("CHANGE ON FRONTDOOR: ");
      Serial.println(frontDoorStatus);
      
      previousFrontDoorStatus = frontDoorStatus;
-     jsonvalue = buildPVCloudValue(laserStatus, z1Status,z2Status, houseStatus, frontDoorStatus);
-     //sendDataToPVCloud("HOUSE_STATUS",jsonvalue, false);
+     jsonvalue = buildPVCloudValue(laserStatus, z1Status,z2Status, houseStatus, frontDoorStatus, temperatureStatus, lightStatus);
+     sendDataToPVCloud("HOUSE_STATUS",jsonvalue, false);
+     lastPVCloudUpdateMillis = millis();
   }
   
+  if(millis() - lastPVCloudUpdateMillis > 60000){
+     jsonvalue = buildPVCloudValue(laserStatus, z1Status,z2Status, houseStatus, frontDoorStatus, temperatureStatus, lightStatus);
+     sendDataToPVCloud("HOUSE_STATUS",jsonvalue, false);   
+     lastPVCloudUpdateMillis = millis(); 
+  }
  
   
   if(isFirstLoop){
@@ -380,7 +482,7 @@ long getDistance(){
     delayMicroseconds(10);
     digitalWrite(pin_usonic_trigger, LOW);
     
-    pulseDuration = pulseIn(pin_usonic_echo, HIGH);
+    pulseDuration = pulseIn(pin_usonic_echo, HIGH,1000);
     
     distanceInCM = (pulseDuration/2)/29.1;
     
@@ -397,7 +499,7 @@ long getLaserDurationFiveInARow(){
     
     for(int i=0; i<10; i++){
        readings_Count ++;
-       laserDuration = pulseIn(pin_laser_01, LOW, 5000);
+       laserDuration = pulseIn(pin_laser_01, LOW, 3000);
        
        if(isLaserDurationWithinSpec(laserDuration)) {
          laserDurationInSpec_Count ++; 
@@ -446,7 +548,7 @@ long getLaserDurationFiveInARow_b(){
     
     for(int i=0; i<10; i++){
        readings_Count_b ++;
-       laserDuration = pulseIn(pin_laser_02, LOW, 5000);
+       laserDuration = pulseIn(pin_laser_02, LOW, 3000);
        
        if(isLaserDurationWithinSpec(laserDuration)) {
          laserDurationInSpec_Count ++; 
@@ -493,12 +595,15 @@ boolean isLaserDurationWithinSpec(long duration){
   return duration > laserDurationSpec_min &&  duration < laserDurationSpec_max;
 }
 
-String buildPVCloudValue(String laserStatus, String z1Status, String z2Status, String houseStatus, String distanceFlag){
-  String pvCloudValue = "{\"per_status\":\""+laserStatus+"\",\"front_door_status\":\""+distanceFlag+"\",\"z1_status\":\""+z1Status+"\",\"z2_status\":\""+z2Status+"\",\"z3_status\":\"H\",\"temp_status\":\"H\",\"light_status\":\"H\",\"house_status\":\""+houseStatus+"\"}";
+String buildPVCloudValue(String laserStatus, String z1Status, String z2Status, String houseStatus, String distanceFlag, String tempStatus, String lightStatus){
+  String pvCloudValue = "{\"per_status\":\""+laserStatus+"\",\"front_door_status\":\""+distanceFlag+"\",\"z1_status\":\""+z1Status+"\",\"z2_status\":\""+z2Status+"\",\"z3_status\":\"H\",\"temp_status\":\""+tempStatus+"\",\"light_status\":\""+lightStatus+"\",\"house_status\":\""+houseStatus+"\"}";
   return pvCloudValue;
 }
 
 void sendDataToPVCloud(String label, String value, boolean waitForResponse){
+  Serial.println("Sending Data to pvCloud");
+  Serial.println(value);
+  
   if(waitForResponse){
     String pvcloudCommand = "node /home/root/pvcloud_api.js action='add_value' value='" + value + "' value_type='JSON_TH' value_label='" + label + "' captured_datetime='2015-03-09+21:00' >> pvcloud_log.txt";
     system ( pvcloudCommand.buffer );
@@ -550,4 +655,12 @@ double readAbsenceSensor(){
 
 double readPresenceSensor(){
    return analogRead( pin_presence );
+}
+
+void tune(){
+  tone(12,NOTE_E6);
+  delay(400);
+  tone (12,NOTE_A6);
+  delay(600);
+  noTone(12);
 }
